c6e664ba574b9bc6192e5f629b7c2909d7ff58ca
ShivarTuplah <ST10256115@vcconnect.edu.za>
2024-05-03 10:58:03 +0200

    Add project files.

---
+++ Models/ContactViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace KhumaloCraft.Models
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Question { get; set; }
+    }
+}
